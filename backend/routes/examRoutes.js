243d0c266384250e688604a9a3f78368c0c35a6a
abobkr ali <abobkracc@gmail.com>
2024-02-14 00:46:28 +0200

    commit

--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -5,10 +5,10 @@ import { protectAdmin, protectUser } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/add-exam',protectAdmin, addExam);
-router.get('/get-exams',protectUser, getExams);
-router.get('/:id/get-exam',protectUser, getExam);
-router.get('/:examId/get-score',protectAdmin, getUserScores);
+router.get('/get-exams', getExams);
+router.get('/:id/get-exam', getExam);
+router.get('/:examId/get-score', getUserScores);
 router.delete('/delete-exam',protectAdmin, deleteExam);
-router.post('/addUserScoreToExam',protectUser, addUserScoreToExam)
+router.post('/addUserScoreToExam', addUserScoreToExam)
 
 export default router;
