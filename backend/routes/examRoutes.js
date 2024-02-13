419b341eaeec4f5b3258d0db9b985a1e8a8a899c
abobkr ali <abobkracc@gmail.com>
2024-02-14 01:53:08 +0200

    last commit

--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/add-exam',protectAdmin, addExam);
 router.get('/get-exams',protectUser, getExams);
 router.get('/:id/get-exam',protectUser, getExam);
-router.get('/:examId/get-score',protectUser, getUserScores);
+router.get('/:examId/get-score', getUserScores);
 router.delete('/delete-exam',protectAdmin, deleteExam);
 router.post('/addUserScoreToExam', addUserScoreToExam)
 
